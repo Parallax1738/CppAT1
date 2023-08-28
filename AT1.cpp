db4fb334c37ee1648a3a179b559f5d549d63cc39
Parallax1738 <Max_demopoulos@outlook.com>
2023-08-28 17:18:43 +1000

    Contact Class Created
    Contact Class has been created with all content, not sure if this is right or not as its mainly ChatGPT, will improve if problems occur or I find a better way.

--- AT1.cpp
+++ AT1.cpp
@@ -1,7 +1,29 @@
 #include "AT1.h"
-#include <iostream>
+// Constructor Implementation
+Contact::Contact(std::string name, bool human, int age) : name(std::move(name)), human(human), age(age) {
+
+}
+// Get Method Implementation
+std::string Contact::getName() const {
+    return name;
+}
+int Contact::getAge() const {
+    return age;
+}
+bool Contact::isHuman() const {
+    return human;
+}
+// Set Method Implementation
+void Contact::setName(const std::string &name) {
+    this->name = name;
+}
+void Contact::setHuman(bool human) {
+    this->human = human;
+}
+void Contact::setAge(int age) {
+    this->age = age;
+}
 // Make a Contact class with three attributes of at least two different data types.
-Contact::
 // Add set and get methods to modify and access the Contact attributes.
 // Add a constructor or otherwise to initialise a Contact.
 // Make a Node class which is a doubly linked node which has a Contact for its data.
--- AT1.h
+++ AT1.h
@@ -1,14 +1,25 @@
 #ifndef ASSIGNMENTS_AT1_H
 #define ASSIGNMENTS_AT1_H
 #include <string>
-//Contact Class
+// Contact Class
 class Contact {
 private:
     // Three Attributes with Two Data Types
     std::string name;
     int age;
-    std::string phoneNumber;
+    bool human;
 public:
-};
+    // Constructor
+    Contact(std::string name, bool human, int age);
 
-#endif
+    // Get Method
+    std::string getName() const;
+    int getAge() const;
+    bool isHuman() const;
+
+    // Set Method
+    void setName(const std::string &name);
+    void setAge(int age);
+    void setHuman(bool human);
+};
+#endif //AT1.h
