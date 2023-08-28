ae903307f115acf76ea7bc8e3e1199436c6a188d
Parallax1738 <Max_demopoulos@outlook.com>
2023-08-28 13:53:05 +1000

    Why Hello There?

---
+++ AT1.cpp
@@ -0,0 +1,9 @@
+#include "AT1.h"
+#include <iostream>
+// Make a Contact class with three attributes of at least two different data types.
+Contact::
+// Add set and get methods to modify and access the Contact attributes.
+// Add a constructor or otherwise to initialise a Contact.
+// Make a Node class which is a doubly linked node which has a Contact for its data.
+// Add functions to link with either the previous or next nodes.
+// Document the Contact and Node classes by documenting the source code.
---
+++ AT1.h
@@ -0,0 +1,14 @@
+#ifndef ASSIGNMENTS_AT1_H
+#define ASSIGNMENTS_AT1_H
+#include <string>
+//Contact Class
+class Contact {
+private:
+    // Three Attributes with Two Data Types
+    std::string name;
+    int age;
+    std::string phoneNumber;
+public:
+};
+
+#endif
